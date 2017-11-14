ea78aa5ef89119d7f9e0f6d1c651ae76152064e5
Iver Osnes <149763@stud.hvl.no>
2017-11-14 10:27:11 +0100

    La til ekstrafunksjon for autogenerering av 5 kunder

--- SentralIver/RegistrerNyKunde.cs
+++ SentralIver/RegistrerNyKunde.cs
@@ -45,6 +45,68 @@ namespace SentralIver
         {
             this.Close();
         }
-        
+
+
+
+
+        // Midlertidig metode for autogenerering av kunder
+        private void btnAuto_Click(object sender, EventArgs e)
+        {
+            string navn, adresse, passord, IP;
+            navn = "Marta"; adresse = "Bergen"; passord = "123abc"; IP = "128.01.12.211";
+            try
+            {
+                sentral.leggTilKunde(navn, adresse, Kid, passord, IP);
+                ++Kid;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            navn = "Eirik"; adresse = "Oslo"; passord = "jegelskerarduino"; IP = "128.01.12.223";
+            try
+            {
+                sentral.leggTilKunde(navn, adresse, Kid, passord, IP);
+                ++Kid;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            navn = "Kent"; adresse = "Minde"; passord = "csgORuler"; IP = "192.01.12.211";
+            try
+            {
+                sentral.leggTilKunde(navn, adresse, Kid, passord, IP);
+                ++Kid;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            navn = "Iver"; adresse = "Ulsteinvik"; passord = "hallaien321"; IP = "172.01.12.556";
+            try
+            {
+                sentral.leggTilKunde(navn, adresse, Kid, passord, IP);
+                ++Kid;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            navn = "Vebjørn"; adresse = "vetikke"; passord = "klassefil123"; IP = "231.01.12.556";
+            try
+            {
+                sentral.leggTilKunde(navn, adresse, Kid, passord, IP);
+                ++Kid;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
     }
 }
